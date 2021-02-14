43fd74d341edd1a09dbdf7c255304f943e8b4d25
Shu Wei Wu <wushuwei@gmail.com>
2021-02-14 16:32:01 -0500

    update go more

--- Test.go
+++ Test.go
@@ -1,23 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/cloudfoundry-community/go-cfenv"
 	service "github.com/cloudnativego/gogo-service/service"
 )
 
 func main() {
-
 	port := os.Getenv("PORT")
-
 	if len(port) == 0 {
-
 		port = "3000"
-
 	}
 
-	server := service.NewServer()
+	appEnv, err := cfenv.Current()
+	if err != nil {
+		fmt.Println("CF Environment not detected.")
+	}
 
+	server := service.NewServer(appEnv)
 	server.Run(":" + port)
-
 }
